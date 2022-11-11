4f1794e45e8aa2c92adb8290a8a144d5b42a93e4
joethyr <joethayr@gmail.com>
2022-11-10 20:32:25 -0700

    add html and js file

---
+++ app.js
@@ -0,0 +1,19 @@
+function Stopwatch {
+  let startTime, endTime, running, duration = 0;
+
+// start
+this.start = function() {
+
+}
+// stop
+this.stop = function() {
+
+}
+// duration
+this.duration = function() {
+  
+}
+
+// reset
+
+}
